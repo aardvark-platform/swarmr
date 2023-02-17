57b910517b942aa8962c79f76fd69a1ea12a8310
Stefan Maierhofer <sm@stefanmaierhofer.com>
2023-02-17 13:44:01 +0100

    RunJobTask (wip)

---
+++ src/Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+switch (args[0])
+{
+    case "create":
+        {
+            var n = int.Parse(args[1]);
+            for (var i = 0; i < n; i++)
+            {
+                var x = Math.Round(Random.Shared.NextDouble() * 999.999, 3);
+                Console.WriteLine(x);
+            }
+            break;
+        }
+
+    default:
+        {
+            var xs = File
+                .ReadAllLines(args[0])
+                .Select(line => decimal.Parse(line))
+                .ToArray()
+                ;
+            Console.WriteLine($"parsed {xs.Length} values");
+            Console.WriteLine($"sum is {xs.Sum()}");
+            break;
+        }
+}
+
