b64883cca29631ec644f48f74b0690918ff63fe4
Stefan Maierhofer <sm@stefanmaierhofer.com>
2023-02-21 12:04:50 +0100

    sync

--- src/Swarmr.Base/Tasks/ScheduleJobTask.cs
+++ src/Swarmr.Base/Tasks/ScheduleJobTask.cs
@@ -15,21 +15,34 @@ public record ScheduleJobTask(string Id, JobConfig Job) : ISwarmTask
     {
         var runJobTask = RunJobTask.Create(Job);
 
-        var idleNodes = context.Nodes.Where(n => n.Status == NodeStatus.Idle);
-        foreach (var n in idleNodes) AnsiConsole.MarkupLine($"[lime][[ScheduleJobTask]] idle node {n.Id}[/]");
-        foreach (var node in idleNodes)
+        // get idle nodes
+        var idleNodes = context.Nodes.Where(n => n.Status == NodeStatus.Idle).ToList();
+
+        // if possible, do not schedule to priary node ...
+        if (idleNodes.Count > 1 && context.PrimaryId != null)
         {
+            idleNodes = idleNodes.Except(context.PrimaryId).ToList();
+            if (idleNodes.Count == 0) throw new Exception("Error 7ff23229-d11a-419c-9dce-2a6799f5264d.");
+        }
+
+        while (idleNodes.Count > 0)
+        {
+            // schedule random node
+            var i = Random.Shared.Next(idleNodes.Count);
+            var scheduledNode = idleNodes[i];
+            idleNodes.RemoveAt(i);
+
             // send task to selected node
-            AnsiConsole.MarkupLine($"[lime][[ScheduleJobTask]] sending job {Id} to node {node.Id} ... [/]");
-            var runJobResponse = await node.Client.RunJobAsync(runJobTask);
+            AnsiConsole.MarkupLine($"[lime][[ScheduleJobTask]] sending job {Id} to node {scheduledNode.Id} ... [/]");
+            var runJobResponse = await scheduledNode.Client.RunJobAsync(runJobTask);
             if (runJobResponse.Accepted)
             {
-                AnsiConsole.MarkupLine($"[lime][[ScheduleJobTask]] sending job {Id} to node {node.Id} ... ACCEPTED[/]");
+                AnsiConsole.MarkupLine($"[lime][[ScheduleJobTask]] sending job {Id} to node {scheduledNode.Id} ... ACCEPTED[/]");
                 return;
             }
             else
             {
-                AnsiConsole.MarkupLine($"[lime][[ScheduleJobTask]] sending job {Id} to node {node.Id} ... JOB REJECTED[/]");
+                AnsiConsole.MarkupLine($"[lime][[ScheduleJobTask]] sending job {Id} to node {scheduledNode.Id} ... JOB REJECTED[/]");
             }
         }
 
